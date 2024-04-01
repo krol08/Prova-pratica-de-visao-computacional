f1ddd30de4e78b426b1ac822404447913fe25b1b
Karoline da Rocha <karolrocha0802@gmail.com>
2024-03-31 22:47:35 -0300

    Update questao_3.py

--- questao_3/questao_3.py
+++ questao_3/questao_3.py
@@ -1,1 +1,31 @@
-sss
+"""
+
+@author: karol
+"""
+
+# Bibliotecas:
+import json
+
+# Caminho dos arquivos JSON.
+caminho_arquivo_1 = 'building/annotation.json'
+caminho_arquivo_2 = 'building/annotation-small.json'
+
+# Abrindo o arquivo "annotation.json", em modo de leitura
+with open(caminho_arquivo_1, 'r') as arquivo:
+    
+    # Carregando os dados lidos.
+    dados_1 = json.load(arquivo)
+    
+# Abrindo o arquivo "annotation-small.json", em modo de leitura
+with open(caminho_arquivo_2, 'r') as arquivo:
+    
+    # Carregando os dados lidos.
+    dados_2 = json.load(arquivo)
+
+info_img = dados_1['images'];
+
+anotacao_img = dados_1['annotations'];
+
+"""
+    Comentario: Resolução incompleta.
+"""
